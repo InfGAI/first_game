de9255e7e90f526f510d4b99caf3147eb09964ed
Anastasiya <infgai@yandex.ru>
2021-03-06 14:48:50 +0300

    add class Ball(generate ball and move it) ball generate by MLB and dispose by MRB

--- main.py
+++ main.py
@@ -9,24 +9,29 @@ class Ball():
         self.pos=(self.pos[0]+1,self.pos[1])
 
     def renew(self):
-        pygame.draw.circle(screen, self.color, self.pos, 50)
+        pygame.draw.circle(screen, self.color, self.pos, 20)
 
 pygame.init()
 FPS=50
 screen=pygame.display.set_mode((600,300))
 clock=pygame.time.Clock()
-first_ball=Ball((100,100),(0,0,0))
-second_ball=Ball((500,100),(0,255,0))
+ball_list=[]
 running=True
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            if event.button==1:
+                ball_list.append(Ball((10,100),(0,0,0)))
+            else:
+                ball_list.pop(0)
     screen.fill((255, 0, 0))
-    first_ball.renew()
-    first_ball.hmove()
-    second_ball.renew()
-    second_ball.hmove()
+    for ball in ball_list:
+        ball.renew()
+        ball.hmove()
+
+
     pygame.display.flip()
     clock.tick(FPS)
 
