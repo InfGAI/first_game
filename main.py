01b4a0c78e576c8fafda38daddc91e6c7108ea42
Anastasiya <infgai@yandex.ru>
2021-03-06 15:16:18 +0300

    add random pos and color

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
+import random
+
+
 class Ball():
-    def __init__(self,pos,color):
-        self.pos=pos # (100,100)
-        self.color=color
+    def __init__(self, pos, color):
+        self.pos = pos  # (100,100)
+        self.color = color
         pygame.draw.circle(screen, self.color, self.pos, 50)
 
     def hmove(self):
-        self.pos=(self.pos[0]+1,self.pos[1])
+        self.pos = (self.pos[0] + 1, self.pos[1])
 
     def renew(self):
         pygame.draw.circle(screen, self.color, self.pos, 20)
@@ -19,11 +22,13 @@ ball_list=[]
 running=True
 while running:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            running=False
-        if event.type==pygame.MOUSEBUTTONDOWN:
-            if event.button==1:
-                ball_list.append(Ball((10,100),(0,0,0)))
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            r, g, b = random.choices(range(0, 256), k=3)
+            if event.button == 1:
+                ball_list.append(Ball((x, y), (r, g, b)))
             else:
                 ball_list.pop(0)
     screen.fill((255, 0, 0))
